c1d4cdcaaf7bd4720b5f06cf0e72355f3a2be87a
lolitafly <390373067@qq.com>
2015-04-28 10:20:59 +0800

    watsons ajax get code

--- Application/Marvel2/Controller/IndexController.class.php
+++ Application/Marvel2/Controller/IndexController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace Marvel2\Controller;
 use Think\Controller;
+header("Content-type: text/html; charset=utf-8");
 class IndexController extends Controller {
     public function index(){
     	//可获奖最低分
@@ -42,16 +43,20 @@ class IndexController extends Controller {
     	}
     }
     
-    public function test(){
-    	dump(D('Kuaidiquan')->getCode());
-    	echo 1;
+    //watsons获得代码
+    public function watsons(){
+//     	$code=D('Kuaidiquan')->watsonsCode();
+    	$code=D('Kuaidiquan')->getCode();
+    	$this->ajaxReturn($code);
     }
     
-    public function add(){
-    	
-    	$data['phone']="13701653154";
-    	$data['score']=500;
-    	$data['ip']=20;
-    	M('User')->add($data);
+    public function showInventory(){
+    	$cdt['id']=array('gt',100000);
+    	$cdt['state']=1;
+    	$rest15=M('kuaidiquan')->where($cdt)->count();
+    	$cdt['id']=array('lt',100000);
+    	$rest10=M('kuaidiquan')->where($cdt)->count();
+    	echo "10元券剩余".$rest10."张\n"."15元券剩余".$rest15."张";
     }
+    
 }
--- Application/Marvel2/Model/KuaidiquanModel.class.php
+++ Application/Marvel2/Model/KuaidiquanModel.class.php
@@ -9,11 +9,16 @@ class KuaidiquanModel extends Model {
 		return $rr;
 	}
 	
+	public function getCodeForSafe(){
+		$cdt['id']=array('gt',100000);
+		$rr=$this->field('id,code,price')->where($cdt)->find();
+		return $rr;
+	}
 	
 	public function getCode1(){
 		//查找发过的
 		$now=time();
-		$hourBefore=$now-1*60*60;//同一IP一小时只能领取一张
+		$hourBefore=$now-3*60*60;//同一IP一小时只能领取一张
 		$cdt['ip']=get_client_ip();
 		$cdt['sendDate']=array('gt',$hourBefore);
 		$cdt['state']=0;
@@ -31,6 +36,40 @@ class KuaidiquanModel extends Model {
 			$cdt['id']=array('elt',100000);
 		}
 		$rr=$this->field('id,code,price')->where($cdt)->find();
+		if(!$rr){
+			return $this->getCodeForSafe();
+		}
+		//更新该券信息
+		unset($cdt);
+		$cdt['id']=$rr['id'];
+		$data['ip']=get_client_ip();
+		$data['sendDate']=time();
+		$data['state']=0;
+		$this->where($cdt)->save($data);
+		
+		return $rr;
+	}
+	
+	public function watsonsCode(){
+		//查找发过的
+		$now=time();
+		$hourBefore=$now-3*60*60;//同一IP一小时只能领取一张
+		$cdt['ip']=get_client_ip();
+		$cdt['sendDate']=array('gt',$hourBefore);
+		$cdt['state']=0;
+		$r=$this->field('id,code,price')->where($cdt)->find();
+		if($r){
+			return $r;
+		}
+		
+		unset($cdt);
+		//查找新的
+		$cdt['state']=1;
+		$cdt['id']=array('gt',100000);
+		$rr=$this->field('id,code,price')->where($cdt)->find();
+		if(!$rr){
+			return $this->getCodeForSafe();
+		}
 		//更新该券信息
 		unset($cdt);
 		$cdt['id']=$rr['id'];
