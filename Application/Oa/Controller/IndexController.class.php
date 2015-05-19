acc27eecafd907547f414dce7135e0867d104dca
lolitafly <390373067@qq.com>
2015-05-20 02:48:18 +0800

    oa wechat login

--- Application/Oa/Controller/IndexController.class.php
+++ Application/Oa/Controller/IndexController.class.php
@@ -4,13 +4,31 @@ use Think\Controller;
 use Com\WechatCorp;
 header("Content-type: text/html; charset=utf-8");
 class IndexController extends Controller {
+	
+	private $userId='';
+	private $deviceId='';
+	
+	/**
+	 * 微信菜单点击无感知登陆
+	 */
     public function index(){
     	if($_GET['code']){
     		$corp=new WechatCorp(C('CorpID'), C('CorpSecret'));
     		$access_token=$corp->getAccessToken();
-    		$user=$corp->getUserInfo($access_token, $_GET['code'],1);
-    		dump($access_token);
-    		dump($user);
+    		$user=$corp->getUserInfo($access_token, $_GET['code'],1);//1为应用id
+    		$cdt['userId']=$user['UserId'];
+    		$cdt['deviceId']=$user['DeviceId'];
+    		
+    		$this->userId=$user['UserId'];
+    		$this->deviceId=$user['DeviceId'];
+    		
+    		$r=M('User')->where($cdt)->find();
+    		if($r){
+    			session('oa_user',$r);
+    			$this->redirect('Project/index');
+    		}else{
+    			$this->redirect('login');
+    		}
     	}else{
 	    	$this->redirect('login');
     	}
@@ -20,7 +38,7 @@ class IndexController extends Controller {
      * 登陆页
      */
     public function login(){
-    	if(cookie('oa_username')){
+    	if(cookie('oa_username')&&$this->userId==''){//PC端登陆
     		$returninfo=D('user')->checkLogin(cookie('oa_username'),cookie('oa_password'),true);
     		if($returninfo['status']==1){
     			$this->redirect('Project/index');
@@ -35,10 +53,16 @@ class IndexController extends Controller {
     public function submit(){
     	$returninfo=D('user')->checkLogin($_POST['username'],$_POST['password'],false);
     	
-    	if($returninfo['status']==1&&$_POST['remember']){
+    	if($returninfo['status']==1&&$_POST['remember']&&$this->userId==''){//PC端登陆成功
     		cookie('oa_username',$_POST['username'],172800);
     		cookie('oa_password',D('user')->pwdEncrypt($_POST['password']),172800);
     	}
+    	if($returninfo['status']==1&&$_POST['remember']&&$this->userId){//移动端登陆
+    		$data['userId']=$this->userId;
+    		$data['deviceId']=$this->deviceId;
+    		M('User')->where('username='.$_POST['username'])->save($data);
+    	}
+    	
     	$this->ajaxReturn($returninfo,'json');
     }
     
@@ -76,8 +100,7 @@ class IndexController extends Controller {
     }
     
     public function test(){
-    	$newData['name']="test111";
-    	D('operate')->saveOperate(4,$newData);
+    	if($this->userId)echo 1;
     }
     
 }
